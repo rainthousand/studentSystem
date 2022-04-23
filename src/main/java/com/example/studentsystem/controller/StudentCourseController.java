95f7ab1125919f4d8c9b44dde2925d4ca40275f1
rainthousand <774848431@qq.com>
2022-04-23 20:25:29 +0100

    course api(awaits tests)

--- src/main/java/com/example/studentsystem/controller/StudentCourseController.java
+++ src/main/java/com/example/studentsystem/controller/StudentCourseController.java
@@ -4,10 +4,16 @@ import com.example.studentsystem.entity.Course;
 import com.example.studentsystem.service.CourseService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
 import java.util.List;
+import java.util.Objects;
 
 @Controller
 @RequestMapping(value = "/student")
@@ -15,12 +21,38 @@ public class StudentCourseController {
     @Resource(name = "courseServiceImpl")
     private CourseService courseService;
 
-    @RequestMapping(value = "/Course")
+    @RequestMapping(value = "/course")
     public String studentAllCourse(Model model) throws Exception {
         List<Course> courseList = courseService.findAllCourse();
         model.addAttribute("courseList",courseList);
 
-        return "student/course";
+        return "course";
     }
 
+    @RequestMapping(value = "/selectedCourse")
+    public String studentAllSelectedCourse(Model model) throws Exception {
+        HttpSession session = ((ServletRequestAttributes) Objects.requireNonNull(RequestContextHolder
+                .getRequestAttributes())).getRequest().getSession();
+        List<Course> selectedCourseList = courseService.findAllCourseByStudentid((Integer) session.getAttribute("username"));
+        model.addAttribute("selectedCourseList",selectedCourseList);
+
+        return "selectedCourse";
+    }
+
+    @RequestMapping("/deleteSelected")
+    public String deleteSelectedCourse(@RequestParam("sid") Integer sid, @RequestParam("cid") Integer cid){
+        courseService.deleteSelectedCourse(sid,cid);
+
+        return "redirect:/selectedCourse";
+    }
+
+    @RequestMapping("/addSelected/{cid}")
+    public String addSelectedCourse(Model model, @PathVariable("cid")Integer cid){
+        HttpSession session = ((ServletRequestAttributes) Objects.requireNonNull(RequestContextHolder
+                .getRequestAttributes())).getRequest().getSession();
+        Integer sid = (Integer) session.getAttribute("username");
+        courseService.addSelectedCourse(sid,cid);
+
+        return "redirect:/selectedCourse";
+    }
 }
