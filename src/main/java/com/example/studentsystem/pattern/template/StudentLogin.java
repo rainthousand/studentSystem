3f2a169c35910b9feb79b00de04c6c766f81799e
LiWeijin <928637382@QQ.COM>
2022-04-26 14:58:57 +0100

    Modified: Implemented login service by TEMPLATE pattern. (#1)

---
+++ src/main/java/com/example/studentsystem/pattern/template/StudentLogin.java
@@ -0,0 +1,23 @@
+package com.example.studentsystem.pattern.template;
+
+import com.example.studentsystem.entity.UserSessionInfo;
+
+import javax.servlet.http.HttpSession;
+import java.util.List;
+
+public class StudentLogin extends LoginProcess {
+    @Override
+    public void setSessionAttribute(HttpSession session, UserSessionInfo userSessionInfo) {
+        session.setAttribute("username", userSessionInfo.getUsername());
+        session.setAttribute("password", userSessionInfo.getPassword());
+        session.setAttribute("registerstatus", userSessionInfo.getRegisterStatus());
+        //设置session过期时间为7200s 默认是1800s，指的是在不进行任何操作的情况下，超时时间，（即若    处于操作时间期间的话，则自动延长超时时间）
+        session.setMaxInactiveInterval(7200);
+//            System.out.println(session.getAttribute("registerstatus"));
+    }
+
+    @Override
+    public String getURL(List<String> URLs) {
+        return URLs.get(2);
+    }
+}
